bf773cc63d0772dce565ac0353fb08fc24f05e54
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-11-04 21:56:56 +0500

    added basic eslint rules

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "warn",
+    },
+  },
+];
---
+++ script.js
@@ -0,0 +1,5 @@
+// ESLint is a tool for identifying and reporting on patterns found in ECMAScript/JavaScript code, with the goal of making code more consistent and avoiding bugs.
+
+const hello = "smfsofsdf";
+
+let helloA = undefined;
